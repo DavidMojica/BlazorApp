8fd832cb3adf8f37f6d78066056cc6b8e454b464
David Mojica <davidmojicav@gmail.com>
2024-09-09 19:53:43 -0500

    First commit

---
+++ Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿namespace RazorApp.Models
+{
+    public class Contact
+    {
+        private string firstName;
+        private string lastName;
+        private string email;
+
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Email { get => email; set => email = value; }
+
+        public Contact(string firstName, string lastName, string email)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+    }
+}
